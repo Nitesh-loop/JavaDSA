58f771f6f758202b1772bf06fa876b2e24a91ba7
Nitesh-loop <nitesh166k@gmail.com>
2022-11-29 00:10:29 +0530

    pattern

--- JavaDSA/src/pattern/pattern5.java
+++ JavaDSA/src/pattern/pattern5.java
@@ -2,33 +2,49 @@ package pattern;
 
 import java.util.Scanner;
 
-public class pattern4 {
+public class pattern5 {
 
 	public static void main(String[] args) {
 		
 		/*
+		  			*		
+		  		*	*	*	
 		  	*	*	*	*	*
-		  		*	*	*	*
-		  			*	*	*
-		  				*	*
-		  					*
+		  		*	*	*	
+		  			*		
 		 * */
+		
+//		(2,1)
+//		(1,3)
+//		(0,5)
+//		(1,3)
+//		(2,1)
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value of n");
 		int n = sc.nextInt();
-		int sp=0, st=n;
+		int sp=n/2, st=1;
 		
 		for(int i=0; i<n; i++) {
 			
 			for(int j=0; j<sp; j++) {
 				System.out.print("\t");
+				
 			}
 			for(int k=0; k<st; k++) {
 				System.out.print("*\t");
+				
 			}
-			sp++;
-			st--;
+			
+			
+			
+			if(i < n/2) {
+				sp--; st+=2;
+			}
+			else {
+				sp++; st-=2;
+			}
+			
 			System.out.println();
 		}
 
