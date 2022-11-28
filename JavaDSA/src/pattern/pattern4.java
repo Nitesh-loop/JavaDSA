407109ecb3b487a7ada99f82a16387e227e0a015
Nitesh-loop <nitesh166k@gmail.com>
2022-11-28 23:25:28 +0530

    new pattern4

--- JavaDSA/src/pattern/pattern4.java
+++ JavaDSA/src/pattern/pattern4.java
@@ -2,22 +2,23 @@ package pattern;
 
 import java.util.Scanner;
 
-public class pattern3 {
+public class pattern4 {
 
 	public static void main(String[] args) {
 		
 		/*
-		  					*
-		  				*	*
-		  			*	*	*
-		  		*	*	*	*
 		  	*	*	*	*	*
+		  		*	*	*	*
+		  			*	*	*
+		  				*	*
+		  					*
 		 * */
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value of n");
 		int n = sc.nextInt();
 
+		
 		for(int i=0; i<n; i++) {
 			for(int j=n-1; j>i; j--) {
 				System.out.print("\t");
